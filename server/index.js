a0feb03f530dc1a7b38f4817dcb4dd55d9a6c501
syuzan-prog <syuzanna.ant@gmail.com>
2022-01-12 19:08:44 +0400

    remove server file.js

--- server/index.js
+++ server/index.js
@@ -1,24 +1,17 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const config = require("config")
-// const cors = require('cors')
 const authRouter = require("./routes/auth.routes")
 const documentRouter = require("./routes/document.routes")
-const fileRouter = require("./routes/file.routes")
-const postsRouter = require("./routes/post.routes")
 const app = express()
 const PORT = config.get('serverPort')
 const corsMiddleware = require('./middleware/cors.middleware')
 
 
 app.use(corsMiddleware)
-// app.use(cors())
 app.use(express.json())
 app.use("/api/auth", authRouter)
 app.use("/api/posts", documentRouter)
-app.use("/api/files", fileRouter)
-
-
 
 const start = async () => {
     try {
